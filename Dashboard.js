d139a8b561fecfd329f9e297e84ff8ea3eaf7d39
Piotr Bogaczyk <139002468+BogaczykPiotr@users.noreply.github.com>
2023-07-17 18:18:42 +0200

    Add current page link hover

--- Dashboard.js
+++ Dashboard.js
@@ -1,1 +1,6 @@
-
+let links = document.querySelectorAll('.link')
+for (let i = 0; i < links.length; i++) {
+	if (links[i].href === window.location.href) {
+		links[i].classList.add('actual-page')
+	}
+}
